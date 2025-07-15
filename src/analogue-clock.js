ca6d8838de2c3a1abcfe6ad3fa96b7cc58c4dff2
David Easley <easleydp@gmail.com>
2025-07-15 16:30:05 +0100

    Initial commit

---
+++ src/analogue-clock.js
@@ -0,0 +1,11 @@
+export class AnalogueClock {
+  constructor(targetElement, options = {}) {
+    if (!(targetElement instanceof HTMLElement)) {
+      throw new Error(
+        "Invalid targetElement provided. Must be an HTMLElement."
+      );
+    }
+    this.targetElement = targetElement;
+    this.options = options;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import { AnalogueClock } from "./analogue-clock.js";
+
+const clockContainer = document.getElementById("clockContainer");
+
+if (clockContainer) {
+  const clock = new AnalogueClock(clockContainer, {});
+} else {
+  console.error("Clock container not found.");
+}
